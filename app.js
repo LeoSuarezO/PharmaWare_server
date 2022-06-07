e56c14a94d33f6e03042935d52884830056613d5
SebSCam <sebas9903@outlook.com>
2022-06-06 22:16:57 -0500

    sql

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
-const express = require('express');
-const mysql = require('mysql');
+const express = require("express");
+const mysql = require("mysql2");
 
-const bodyparser = require('body-parser');
+const bodyparser = require("body-parser");
 
-const PORT = process.env.port || 3080;
+const PORT = process.env.port || 3000;
 
 const app = express();
 
-app.use(bodyparser.urlencoded({ extended: false}));
+app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json);
 
 var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'me',
-    password : 'secret',
-    database : 'my_db'
-  });
+  host: "bkr8uzegu9z4k52mgvt3-mysql.services.clever-cloud.com",
+  user: "u24sbkla20yreep0",
+  password: "mW8vDttJLo7NGu4oy6nZ",
+  database: "bkr8uzegu9z4k52mgvt3",
+  uri: "mysql://u24sbkla20yreep0:mW8vDttJLo7NGu4oy6nZ@bkr8uzegu9z4k52mgvt3-mysql.services.clever-cloud.com:3306/bkr8uzegu9z4k52mgvt3"
+});
 
-  connection.connect();
+//Chek connect.
+connection.connect((error) => {
+  if (error) throw error;
+  console.log("Database server running...");
+});
 
-  //Chek connect.
-  connection.connect(error => {
-      if(error) throw error;
-      console.log('Database server running...')
-  });
+const tables = connection.query("show tables",(err, data)=>{
+    console.log(data)
+})
 
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}...`));
-
+app.listen(PORT, () => console.log(`Server running on port ${PORT}...`));
