a1e59cf59b6c96ccb8a2a01a35bd4586afae0aeb
SebSCam <sebas9903@outlook.com>
2022-07-07 23:09:18 -0500

    db test

--- src/test/db.test.js
+++ src/test/db.test.js
@@ -1,5 +1,11 @@
-const { expectCt } = require("helmet");
 const db = require("../database");
+const supertest = require('supertest')
+const {app} = require('../index')
+
+const api = supertest(app)
+
+let admin_token
+let user_token
 
 describe("DB Connection", () => {
   it("Verify DB status avaliable", async () => {
@@ -7,3 +13,29 @@ describe("DB Connection", () => {
     expect(validate).toBe(true);
   });
 });
+
+describe("DB Query Test", () => {
+    it("User Query", async () => {
+      const validate = (await db.query("SELECT * FROM PRODUCTOS")) != null;
+      expect(validate).toBe(true);
+    });
+  });
+  
+describe('Authentication Test',()=>{
+    it('Obtain token', async ()=>{
+        const res = await api.post('/auth/login').send({
+            "username":"admin",
+            "password":"admin"
+        })
+        admin_token = res.body.token != null;
+        expect(admin_token).toBe(true);
+    })  
+})
+
+describe('Api Inventory Test',()=>{
+    it('Obtain Products', async ()=>{
+        const data = await api.get('/inventory/products')
+        let products = data != null;
+        expect(products).toBe(true);
+    })  
+})
