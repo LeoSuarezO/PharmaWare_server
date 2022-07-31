bad2301a56f1e1589e3c7c722409cd659066ad79
Fabián Ojeda <67116346+Fabian-Ojeda@users.noreply.github.com>
2022-07-31 01:25:00 -0500

    change to post

--- src/routes/report.routes.js
+++ src/routes/report.routes.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const controller = require('../controllers/report.controller')
 
-router.get('/daily', controller.dailyReport)
-router.get('/monthly', controller.monthlyReport)
-router.get('/anual', controller.anualReport)
+router.post('/daily', controller.dailyReport)
+router.post('/monthly', controller.monthlyReport)
+router.post('/anual', controller.anualReport)
 
 module.exports = router
