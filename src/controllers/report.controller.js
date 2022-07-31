23da5aace558c83fedd8acbf44d8dbd170089058
SebSCam <sebas9903@outlook.com>
2022-07-30 21:39:03 -0500

    daily report

--- src/controllers/report.controller.js
+++ src/controllers/report.controller.js
@@ -65,25 +65,25 @@ export const monthlyReport = async (req, res) => {
 export const dailyReport = async (req, res) => {
     const date = req.body.date;
   const result = await db.query(
-    `SELECT DN.total total19, CN.total total5, CR.total total0 FROM
+    `SELECT DN.total total19, CN.total total5, CR.total total0  FROM 
     (SELECT SUM(I.valor_total) total
-    FROM bkr8uzegu9z4k52mgvt3.VENTAS V,  bkr8uzegu9z4k52mgvt3.ITEM_VENTA I, bkr8uzegu9z4k52mgvt3.PRODUCTOS P
-    WHERE DATE_FORMAT(V.fecha, 'd%-%m-%Y') = ?
+    FROM bkr8uzegu9z4k52mgvt3.VENTAS V, bkr8uzegu9z4k52mgvt3.ITEM_VENTA I, bkr8uzegu9z4k52mgvt3.PRODUCTOS P
+    WHERE V.fecha = ?
     AND I.id_venta = V.id_venta
     AND I.id_producto = P.id_producto
-    AND P.grabado_impuesto = 19) DN,
+    AND P.grabado_impuesto=19) DN,
     (SELECT SUM(I.valor_total) total
-    FROM bkr8uzegu9z4k52mgvt3.VENTAS V,  bkr8uzegu9z4k52mgvt3.ITEM_VENTA I, bkr8uzegu9z4k52mgvt3.PRODUCTOS P
-    WHERE DATE_FORMAT(V.fecha, 'd%-%m-%Y') = ?
+    FROM bkr8uzegu9z4k52mgvt3.VENTAS V, bkr8uzegu9z4k52mgvt3.ITEM_VENTA I, bkr8uzegu9z4k52mgvt3.PRODUCTOS P
+    WHERE V.fecha = ?
     AND I.id_venta = V.id_venta
     AND I.id_producto = P.id_producto
-    AND P.grabado_impuesto = 5) CN,
+    AND P.grabado_impuesto=5) CN,
     (SELECT SUM(I.valor_total) total
-    FROM bkr8uzegu9z4k52mgvt3.VENTAS V,  bkr8uzegu9z4k52mgvt3.ITEM_VENTA I, bkr8uzegu9z4k52mgvt3.PRODUCTOS P
-    WHERE DATE_FORMAT(V.fecha, 'd%-%m-%Y') = ?
+    FROM bkr8uzegu9z4k52mgvt3.VENTAS V, bkr8uzegu9z4k52mgvt3.ITEM_VENTA I, bkr8uzegu9z4k52mgvt3.PRODUCTOS P
+    WHERE V.fecha = ?
     AND I.id_venta = V.id_venta
     AND I.id_producto = P.id_producto
-    AND P.grabado_impuesto = 0) CR`,
+    AND P.grabado_impuesto=0) CR;`,
     [date, date, date]
   );
   if (result[0]) {
