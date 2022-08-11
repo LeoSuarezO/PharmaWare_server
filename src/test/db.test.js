d82be752f961fe508aebe787d8c27a54b95477cf
SebSCam <sebas9903@outlook.com>
2022-08-10 20:39:45 -0500

    test

--- src/test/db.test.js
+++ src/test/db.test.js
@@ -1,11 +1,12 @@
+import 'babel-polyfill'
 const db = require("../database");
-const supertest = require('supertest')
-const {app} = require('../index')
+const supertest = require("supertest");
+const { app, server } = require("../index");
 
-const api = supertest(app)
+const api = supertest(app);
 
-let admin_token
-let user_token
+let admin_token;
+let user_token;
 
 describe("DB Connection", () => {
   it("Verify DB status avaliable", async () => {
@@ -15,27 +16,30 @@ describe("DB Connection", () => {
 });
 
 describe("DB Query Test", () => {
-    it("User Query", async () => {
-      const validate = (await db.query("SELECT * FROM PRODUCTOS")) != null;
-      expect(validate).toBe(true);
-    });
+  it("User Query", async () => {
+    const validate = (await db.query("SELECT * FROM PRODUCTOS")) != null;
+    expect(validate).toBe(true);
   });
-  
-describe('Authentication Test',()=>{
-    it('Obtain token', async ()=>{
-        const res = await api.post('/auth/login').send({
-            "username":"admin",
-            "password":"admin"
-        })
-        admin_token = res.body.token != null;
-        expect(admin_token).toBe(true);
-    })  
-})
+});
 
-describe('Api Inventory Test',()=>{
-    it('Obtain Products', async ()=>{
-        const data = await api.get('/inventory/products')
-        let products = data != null;
-        expect(products).toBe(true);
-    })  
-})
+describe("Authentication Test", () => {
+  it("Obtain token", async () => {
+    const res = await api.post("/auth/login").send({
+      username: "admin",
+      password: "admin",
+    });
+    expect(res.status).toBe(200);
+  });
+});
+
+describe("Api Inventory Test", () => {
+  it("Obtain Products", async () => {
+    const data = await api.get("/inventory/products");
+    let products = data != null;
+    expect(products).toBe(true);
+  });
+
+  afterAll(async () => {
+    await server.close()
+  });
+});
