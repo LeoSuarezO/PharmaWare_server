23fd5b57e3a453733900c887e16368070fdf9c76
SebSCam <sebas9903@outlook.com>
2022-08-12 00:24:50 -0500

    update

--- src/routes/formats.routes.js
+++ src/routes/formats.routes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const controller = require("../controllers/formats.controller");
 
 router.post("/temp_humidity", controller.tempAndHumidity);
-router.get("/temp_humidity", controller.getFormatTempHum);
+router.post("/get_temp_humidity", controller.getFormatTempHum);
 
 module.exports = router;
